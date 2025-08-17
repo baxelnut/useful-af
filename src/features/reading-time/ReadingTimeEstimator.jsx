35e0dede1c2e115df3b461bd60be3bd294370e56
Basilius Tengang <basiliustengang24@gmail.com>
2025-08-17 15:04:56 +0700

    ReadingTime Estimator done (ez)

--- src/features/reading-time/ReadingTimeEstimator.jsx
+++ src/features/reading-time/ReadingTimeEstimator.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
 // Style
 import "./ReadingTimeEstimator.css";
+// Components
+import Button from "../../components/button/Button";
+import Input from "../../components/input/Input";
 
 export default function ReadingTimeEstimator() {
+  const [text, setText] = useState("");
+
+  // calculate words properly
+  const wordCount = text.trim().split(/\s+/).filter(Boolean).length;
+  const wpm = 200; // average reading speed
+  const readingTimeMinutes = Math.ceil(wordCount / wpm);
+
   return (
-    <div className="reading-time">
-      <h1>ReadingTimeEstimator</h1>
+    <div className="reading-time-estimator">
+      <div className="reading-time-result">
+        <p>
+          <em>Estimated reading time:</em>{" "}
+          <strong>{readingTimeMinutes} min</strong>
+        </p>
+      </div>
+
+      <Input
+        type="text"
+        placeholder="Type or paste your text here..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        isTextarea
+        minHeight="420px"
+        fullWidth
+      />
+
+      <Button
+        text="Clear"
+        onClick={() => setText("")}
+        backgroundColor="var(--primary)"
+        short
+      />
     </div>
   );
 }
