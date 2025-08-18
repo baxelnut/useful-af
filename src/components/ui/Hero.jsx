15a13c142e082201eb38e26153f23a1acd738069
Basilius Tengang <basiliustengang24@gmail.com>
2025-08-18 10:33:16 +0700

    greet the guest in ghetto university

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -2,15 +2,18 @@
 import "./Hero.css";
 // Components
 import TabsContainer from "./TabsContainer";
+// Data
+import { SAVAGE_GREETINGZ } from "../../data/utilsData";
 
 export default function Hero({ featuresRoutes, selected, setSelected }) {
   const heroImage = selected?.heroImage || "/stock/oil-black-1.webp"; // fallback
+  const heroTitle = selected
+    ? selected.label
+    : SAVAGE_GREETINGZ[Math.floor(Math.random() * SAVAGE_GREETINGZ.length)];
 
   return (
     <div className="hero" style={{ backgroundImage: `url(${heroImage})` }}>
-      <h2 className="hero-title">
-        {selected ? selected.label : "Yo whatchu want fool?👇"}
-      </h2>
+      <h2 className="hero-title">{heroTitle}</h2>
       <TabsContainer
         featuresRoutes={featuresRoutes}
         selected={selected}
